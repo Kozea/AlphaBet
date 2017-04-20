a93e0ef8f806ba67837cfbe5f4b808668cd07175
Romain ROUVIER <romain.rouvier@kozea.fr>
2017-04-20 14:50:11 +0200

    Adding data for the current matchday and list of matchdays

--- alphabet/alphabet.py
+++ alphabet/alphabet.py
@@ -3,6 +3,7 @@ import http.client
 import json
 import os
 import sqlite3
+import time
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
      
@@ -61,4 +62,21 @@ def index():
     connection_otherdatas.request('GET', '/v1/competitions/434/fixtures', None, headers )
     response_maindatas = json.loads(connection_maindatas.getresponse().read().decode())
     response_otherdatas = json.loads(connection_otherdatas.getresponse().read().decode())
-    return render_template('page.html', currentmatchday=response_maindatas['currentMatchday'], competitions=response_maindatas['caption'],fixtures_datas=response_otherdatas['fixtures'])
+    return render_template('page.html',numberofmatchdays=response_maindatas['numberOfMatchdays'], currentmatchday=response_maindatas['currentMatchday'], competitions=response_maindatas['caption'],fixtures_datas=response_otherdatas['fixtures'])
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    

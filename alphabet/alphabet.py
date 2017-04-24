bfacf292327a504a4ae7f3d53b384cfa18a78d57
Romain ROUVIER <romain.rouvier@kozea.fr>
2017-04-24 11:29:30 +0200

    Retrieve and display match IDs

--- alphabet/alphabet.py
+++ alphabet/alphabet.py
@@ -4,9 +4,10 @@ import json
 import os
 import sqlite3
 import time
+import pprint
+
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
-     
 app = Flask(__name__) # create the application instance 
 app.config.from_object(__name__) # load config from this file , alphabet.py
 
@@ -69,7 +70,11 @@ def index():
     connection_otherdatas.request('GET', '/v1/competitions/434/fixtures', None, headers )
     response_maindatas = json.loads(connection_maindatas.getresponse().read().decode())
     response_otherdatas = json.loads(connection_otherdatas.getresponse().read().decode())
-    return render_template('page.html', urlusername=urlusername ,users=users, matchdaynumber=matchdaynumber, numberofmatchdays=response_maindatas['numberOfMatchdays'], currentmatchday=response_maindatas['currentMatchday'], competitions=response_maindatas['caption'],fixtures_datas=response_otherdatas['fixtures'])
+    fixtures_datas=response_otherdatas['fixtures']
+    matchid=response_otherdatas['fixtures'][0]['id']
+    pprint.pprint(matchid)
+
+    return render_template('page.html', urlusername=urlusername ,users=users, matchdaynumber=matchdaynumber, numberofmatchdays=response_maindatas['numberOfMatchdays'], currentmatchday=response_maindatas['currentMatchday'], competitions=response_maindatas['caption'],fixtures_datas=fixtures_datas)
 
 
 @app.route('/login', methods=['GET','POST'])
